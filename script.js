83f6683abec508e8f4762d846e12c6901c55c15c
yusufhemn79-star <yusufhemn79@gmail.com>
2026-08-16 17:03:07 +0300

    Add button click alerts in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").onclick = function () {
+  alert("Hello! 👋");
+};const button = document.getElementById("btn");
+
+button.onclick = function () {
+  alert("Welcome to my website! 🚀");
+};const contactButton = document.getElementById("contactBtn");
+
+contactButton.addEventListener("click", function () {
+  alert("Hello Yusuf! 👋 Thanks for visiting my website.");
+});
